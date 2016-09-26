2cbd04bf7910a7068e380000d3b3986790cec210
Siddhant Shah <siddhant@siddhant.design>
2016-09-26 11:55:08 -0400

    Updated first page

--- uiWebsite/module.js
+++ uiWebsite/module.js
@@ -2,7 +2,7 @@ var uiApp = angular.module('uiApp',['ngRoute']);
 uiApp.config(function($routeProvider){
   $routeProvider
   .when("/content",{
-      templateUrl: 'Views/ContentView.html'
+      templateUrl: 'uiWebsite/Views/ContentView/ContentView.html'
   })
   .otherwise('/');
 
