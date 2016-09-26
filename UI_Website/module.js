20a489df5ca70e548bb8d88fdd152aec01a4a11b
Siddhant Shah <siddhant@siddhant.design>
2016-09-26 12:02:56 -0400

    changed

--- UI_Website/module.js
+++ UI_Website/module.js
@@ -2,7 +2,7 @@ var uiApp = angular.module('uiApp',['ngRoute']);
 uiApp.config(function($routeProvider){
   $routeProvider
   .when("/content",{
-      templateUrl: 'uiWebsite/Views/ContentView/ContentView.html'
+      templateUrl: 'UI_Website/Views/ContentView/ContentView.html'
   })
   .otherwise('/');
 
